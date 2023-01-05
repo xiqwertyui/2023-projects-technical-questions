9838533e68ee603a6dbdda7db6efb7d8f96233b0
KevinXian <Xiqwertyui@gmail.com>
2023-01-06 00:32:13 +1100

    basically solved the problem

---
+++ q2-backend/ts_template/roundupper_100.test.ts
@@ -0,0 +1,116 @@
+import request from 'sync-request';
+import {HOST, PORT} from './config.json'
+
+const OK = 200;
+const SuccessfulResponses = {
+  lower: 200,
+  upper: 299
+};
+
+const root = `${HOST}:${PORT}`;
+
+const advanceRequest = (method : ('GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH'), route : string, parameters : object) => {
+  const res = request(
+    method,
+    root + route,
+    (method === 'GET' || method === 'DELETE') ? { qs: parameters } : { json: parameters }
+  );
+  return JSON.parse(res.getBody() as string);
+};
+
+
+function clear() {
+  return advanceRequest("DELETE", "/clear", {});
+}
+
+
+beforeEach(()=>clear());
+
+describe('/entity',()=>{
+  const entity = {
+    "entities": [
+        {
+            "type": "space_cowboy",
+            "metadata": {
+                "name": "Jim",
+                "lassoLength": 1 
+            },
+            "location": {
+                "x": 0,
+                "y": 0
+            }
+        },
+        {
+            "type": "space_cowboy",
+            "metadata": {
+                "name": "Bob",
+                "lassoLength": 2 
+            },
+            "location": {
+                "x": 0,
+                "y": 0
+            }
+        },
+        {
+            "type": "space_animal",
+            "metadata": {
+                "type": "flying_burger"
+            },
+            "location": {
+                "x": 0,
+                "y": 1
+            }
+        },
+        {
+          "type": "space_animal",
+          "metadata": {
+              "type": "pig"
+          },
+          "location": {
+              "x": 0,
+              "y": 2
+          }
+        },
+        {
+          "type": "space_animal",
+          "metadata": {
+              "type": "dragon"
+          },
+          "location": {
+              "x": 0,
+              "y": 3
+          }
+        },
+    ]
+  }
+  const query  = {
+    "cowboy_name": "Bob" 
+  }
+  test('/entity',()=>{
+   
+    expect(advanceRequest("POST", '/entity', entity)).toStrictEqual({});
+    
+  })
+
+  test ('/la', ()=>{
+    expect(advanceRequest("POST", '/entity', entity)).toStrictEqual({});
+    expect(advanceRequest("GET", '/lassoable', query).space_animals).toStrictEqual(
+      expect.arrayContaining([
+        {
+            "type": "pig",
+            "location": {
+                "x": 0,
+                "y": 2
+            } 
+        },
+        {
+            "type": "flying_burger",
+            "location": {
+                "x": 0,
+                "y": 1
+            } 
+        }
+    ]))
+  })
+})
+
--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -1,4 +1,14 @@
-import express from 'express';
+// import express, { query } from 'express';
+
+import express, { json, Request, Response } from 'express';
+import cors from 'cors';
+// OPTIONAL: Use middleware to log (print to terminal) incoming HTTP requests
+import morgan from 'morgan';
+
+import {HOST, PORT} from './config.json'
+
+
+// export const root = `${HOST}:${PORT}`;
 
 // location is the simple (x, y) coordinates of an entity within the system
 // spaceCowboy models a cowboy in our super amazing system
@@ -16,17 +26,77 @@ type spaceEntity =
 // === ADD YOUR CODE BELOW :D ===
 
 // === ExpressJS setup + Server setup ===
-const spaceDatabase = [] as spaceEntity[];
+let spaceDatabase = [] as spaceEntity[];
 const app = express();
+// Use middleware that allows for access from other domains (needed for frontend to connect)
+app.use(cors());
+// Use middleware that allows us to access the JSON body of requests
+app.use(json());
+// (OPTIONAL) Use middleware to log (print to terminal) incoming HTTP requests
+app.use(morgan('dev'));
 
-// the POST /entity endpoint adds an entity to your global space database
+
+// // the POST /entity endpoint adds an entity to your global space database
 app.post('/entity', (req, res) => {
-    // TODO: fill me in
+    const { entities } = req.body;
+    const jsonRes = dataBaseAdd(entities);
+    console.log(spaceDatabase);
+    return res.json(jsonRes);
 });
 
 // lasooable returns all the space animals a space cowboy can lasso given their name
 app.get('/lassoable', (req, res) => {
-    // TODO: fill me in
+    const cowboy_name = req.query.cowboy_name as string;  //http://localhost:8080/lassoable?cowboy_name=Bob
+    return res.json(dataBaseGetLassoableAnimal(cowboy_name));
 })
 
-app.listen(8080);
+app.delete('/clear', (req, res) => {
+    return res.json(clear());
+})
+
+
+/**
+ * Start server
+ */
+const server = app.listen(PORT, () => {
+    console.log(`Starting Express Server at the URL: '${HOST}:${PORT}'`);
+  });
+  
+/**
+ * Handle Ctrl+C gracefully
+ */
+process.on('SIGINT', () => {
+server.close(() => {
+    console.log('Shutting down server gracefully.');
+    process.exit();
+});
+});
+
+
+export function dataBaseAdd(entities: spaceEntity []) {
+    spaceDatabase = spaceDatabase.concat(entities);
+    return {};
+}
+
+export function dataBaseGetLassoableAnimal(cowBoyName: string) {
+    const allCowBoyInfo = spaceDatabase.filter(entry=>entry.type === "space_cowboy") as any as { type: "space_cowboy", metadata: spaceCowboy, location: location } [];
+    const cowBoyInfo = allCowBoyInfo.find(entry=>entry.metadata.name === cowBoyName) as { type: "space_cowboy", metadata: spaceCowboy, location: location };
+    const space_animals = spaceDatabase
+        .filter(entry=>entry.type === "space_animal")
+        .filter(animalInfo=>{
+            console.log(cowBoyInfo.metadata.lassoLength, cowBoyInfo.location, animalInfo.location)
+            return isLassoable(cowBoyInfo.metadata.lassoLength, cowBoyInfo.location, animalInfo.location)
+        })
+    console.log(space_animals)
+    return {space_animals};
+}
+
+export function isLassoable(cowBoyLassoLength: number, cowBoyLocation: location, animalLocation: location) : boolean {
+    const distance = Math.sqrt((cowBoyLocation.x-animalLocation.x)**2+(cowBoyLocation.y-animalLocation.y)**2);
+    return cowBoyLassoLength >= distance;
+}
+
+export function clear() {
+    spaceDatabase = [];
+    return {};
+}
